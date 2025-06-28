235d58f9071bdc625c15fb071a8683a4884f81d7
mnelson104 <mnelson104@gmail.com>
2025-06-27 23:41:14 -0400

    Updated both files from Ch 2 work

--- coffee.py
+++ coffee.py
@@ -1,6 +1,35 @@
 # ClydeBank Coffee Shop Simulator 4000
 # Copyright (C) 2025  ClydeBank Media, All Rights Reserved
+# Current day number
+day = 1
+# Sales list of dictionaries
+# sales = [
+#     {
+#          "day": 1,
+#          "coffe_inv": 100,
+#          "advertising": "10",
+#          "temp": 68,
+#         "cups_siold": 16
+#    },
+#     {
+#          "day": 2,
+#          "coffe_inv": 84,
+#          "advertising": "15",
+#          "temp": 72,
+#         "cups_siold": 20
+#    },
+#     {
+#          "day": 3,
+#          "coffe_inv": 64,
+#          "advertising": "5",
+#          "temp": 78,
+#         "cups_siold": 10
+#    },
+# ]
+# Create an empty sales list
+sales = []
 
+# Print the welcome message
 print("ClydeBank Coffee Shop Simulator 4000, Version 1.00")
 print("Copyright (C) 2025 ClydeBank Media, All Rights Reserved.\n")
 print("Let's collect some infrmation before we start the game.\n")
@@ -17,4 +46,4 @@ cup_price = input("What do you want to charge per cup of coffee? ")
 # Display what we have
 print("\nGreat. Here's what we've collected so far.\n")
 print("Your name is " + name + " and you're opening " + shop_name + ".")
-print("Your first cup of coffee wil sell for $" + cup_price + ".\n")
+print("Your first cup of coffee will sell for $" + cup_price + ".\n")
--- scratch.py
+++ scratch.py
@@ -1,2 +1,47 @@
 fname = input("what is your first name?")
 print("The first letter of your name is " + fname[0])
+
+# This is an example of a list
+# Lists are mutable, meaning they can be changed
+grocery_list = ["eggs", "milk", "cheese", "pasta"]
+print("The first item on your grocery list is " + grocery_list[0])
+print("The last item on your grocery list is " + grocery_list[-1])
+
+# This is an example of a tuple
+# Tuples are immutable, meaning they cannot be changed
+planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+print("The third planet from the sun is ... " + planets[3])
+
+## This is an example of a set
+# Sets are unordered collections of unique items
+customers = {
+    "James Smith", 
+    "Andrea Richards", 
+    "Sam Sharp", 
+    "Brenda Longmire", 
+    "Veronica Marsh", 
+    "Sylvia Smith", 
+    "James Smith", 
+    "Venesa Bush", 
+    "Steve Hammersmith", 
+    "Brenda Longmire", 
+    "Sylvia Smith", 
+    "Steve Hammersmith", 
+    "Walt Hawkins"}
+print(customers)
+
+# This is an example of a dictionary
+# Dictionaries are collections of key-value pairs
+customer1 = {
+    "name": "James Smith",
+    "age": 24,
+    "phone": "555-555-1941",
+    "email": "james@xyzinternet.com"
+}
+customer2 = {
+    "name": "Andrea Richards",
+    "age": 33,
+    "phone": "555-555-4928",
+    "email": "andrea@coffeeloversunite.us"
+}
+print(customer1["name"])
