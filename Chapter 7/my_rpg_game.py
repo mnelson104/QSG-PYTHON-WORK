c9d124415030eba210e15659ad139abdceab6848
mnelson104 <mnelson104@gmail.com>
2025-09-25 22:33:19 -0400

    Set up future work for ch6. Added ch7 work.

---
+++ Chapter 7/my_rpg_game.py
@@ -0,0 +1,100 @@
+# Class setup for a simple RPG system
+
+class PlayerClass:
+    pass
+
+# The brave fighter!
+class Fighter(PlayerClass):
+    pass
+
+# The noble Paladin!
+class Paladin(PlayerClass):
+    pass
+
+# The wild sorcerer!
+class Sorcerer(PlayerClass):
+    pass
+
+# The Druid of the forest!
+class Druid(PlayerClass):
+    pass
+
+# The team healer!
+class Healer(PlayerClass):
+    pass
+
+#The player class
+class Player:
+    pass
+
+# The weapon class
+class Weapon:
+    pass
+
+# The short sword weapon
+class ShortSword(Weapon):
+    pass
+
+# The long sword weapon
+class LongSword(Weapon):
+    pass
+
+# The halaberd weapon
+class Halaberd(Weapon):
+    pass
+
+# The Armor class
+class Armor:
+    pass
+
+# Leather armor
+class LeatherArmor(Armor):
+    pass
+
+# Half plate armor
+class HalfPlateArmor(Armor):
+    pass    
+
+# Full plate armor
+class FullPlateArmor(Armor):
+    pass
+
+# Chain male armor
+class ChainMailArmor(Armor):
+    pass
+
+# A shield
+class Shield(Armor):
+    pass
+
+# Magic spell class
+class Spell:
+    pass
+
+# Fireball spell
+class Fireball(Spell):
+    pass
+
+# Heal spell
+class Heal(Spell):
+    pass
+
+# Now to add some monsters!
+class Monster:
+    pass
+
+# Goblin monster
+class Goblin(Monster):
+    pass
+
+# Troll monster
+class Troll(Monster):
+    pass
+
+# Mind flayer monster
+class MindFlayer(Monster):
+    pass
+
+# Dragon monster
+class Dragon(Monster):
+    pass
